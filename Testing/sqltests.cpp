6ea1f2309c6288c49aca222d4a5bfa41cd525626
Minjaben <Benjamin@Minjaben.ch>
2023-05-29 23:54:52 +0200

    changed test verbosity and fixed it

--- Testing/sqltests.cpp
+++ Testing/sqltests.cpp
@@ -1,13 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-// #include "../includes/lib1/functions.hpp"
 #include <SqliteInterface.hpp>
 
-// Write UNIT Test for GetDevices function result should be 1. 
-
-TEST_CASE("GetDevices returns 1", "[GetDevices]") {
+TEST_CASE("GetDevices returns 0, if not, database or Devices table missing or general sql error", "[GetDevices]") {
   vdb db;
   std::string device_str;
-  REQUIRE(db.GetDevices(device_str) == 1);
+  REQUIRE(db.GetDevices(device_str) == 0);
 }
+
