f1b97567d7124b92c9a3f591d243a801f0efec8b
Minjaben <Benjamin@Minjaben.ch>
2023-05-30 02:23:17 +0200

    adds test for creation of database

--- Testing/sqltests.cpp
+++ Testing/sqltests.cpp
@@ -1,7 +1,17 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
+#include <filesystem>
 
 #include <SqliteInterface.hpp>
+#include <sql_access.hpp>
+
+TEST_CASE("SqliteInterface constructor creates database file", "[sql_access]") {
+
+  std::shared_ptr<sql_access> db = std::make_shared<sql_access>("test");
+
+  REQUIRE(std::filesystem::exists("test.db"));
+}
+
 
 TEST_CASE("GetDevices returns 0, if not, database or Devices table missing or general sql error", "[GetDevices]") {
   vdb db;
