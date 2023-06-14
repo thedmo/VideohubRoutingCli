4399cb651739ed4d441994da82bcb5ed64e5a090
kara <benjaminehauser@gmail.com>
2023-06-14 16:39:18 +0200

    adds acquisition of mock server for testing

---
+++ scripts/GetSimulator.ps1
@@ -0,0 +1,21 @@
+$filePath = ".\VideohubSimulator"
+$targetFile = "BmdVideoHub.exe"
+$zipName = "VideohubSimulator_v1.0.1_debug.zip"
+$url = "https://github.com/thedmo/VideoHub-Simulator/releases/download/v1.0.1/debug.zip"
+
+if (Test-Path -Path $filePath) {
+    Write-Host "Simulator already downloaded..."
+}
+else {
+    Write-Host "Creating directory..."
+    New-Item -ItemType Directory -Path $filePath | Out-Null
+
+    Write-Host "Downloading Videohub Simulator..."
+    Invoke-WebRequest -Uri $url -OutFile $zipName
+    
+    Write-Host "Unzipping archive..."
+    Expand-Archive -Path ".\$zipName" -DestinationPath $filePath
+    
+    Write-Host "Cleaning up..."
+    Remove-Item $zipName
+}
