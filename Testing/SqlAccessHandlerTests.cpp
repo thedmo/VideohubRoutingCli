a1bd32873c44c89cfd4f969d33414329314be410
kara <benjaminehauser@gmail.com>
2023-07-21 09:44:06 +0200

    moves values for tests into constants and adds test to load data

--- Testing/SqlAccessHandlerTests.cpp
+++ Testing/SqlAccessHandlerTests.cpp
@@ -14,24 +14,31 @@ const std::string INTEGER = "INT";
 const std::string TEXT = "VARCHAR";
 const std::string BLOB = "BLOB";
 
-std::string col1 = "col1";
-std::string col2 = "col2";
-std::string col3 = "col3";
-std::string col4 = "col4";
-std::string col5 = "col5";
-std::string col6 = "col6";
+const std::string colid = "colid";
+const std::string colStr = "colStr";
+const std::string colInt = "colInt";
+const std::string colVecPair = "colVecPair";
+const std::string colVecString = "colVecString";
+const std::string colVecInt = "colVecInt";
 
-std::string row1 = "row1";
-std::string row2 = "row2";
-std::string row3 = "row3";
+const std::string row1 = "row1";
+const std::string row2 = "row2";
+const std::string row3 = "row3";
 
-std::string str1 = "one";
-std::string str2 = "two";
-std::string str3 = "three";
+const std::string str1 = "one";
+const std::string str2 = "two";
+const std::string str3 = "three";
 
-std::vector <std::string> strVec1 = { str1, str2, str3 };
-std::vector <int> intVec1 = { 1,2,3 };
-std::vector <std::pair<int, int>> pairVec1 = { { 1, 2 },{ 3, 4 },{ 5, 6 } };
+const int int1 = 1;
+const int int2 = 2;
+const int int3 = 3;
+const int int4 = 4;
+const int int5 = 5;
+const int int6 = 6;
+
+const std::vector <std::string> strVec1 = { str1, str2, str3 };
+const std::vector <int> intVec1 = { int1,int2,int3 };
+const std::vector <std::pair<int, int>> pairVec1 = { { int1, int2 },{ int3, int4 },{ int5, int6 } };
 
 void DropTables() {
 	int result = 0;
@@ -84,9 +91,9 @@ TEST_CASE("Add Table Primary function") {
 
 	int result = SqliteHandler::DbMod::CreateTableWithPrimaryKey(
 		DB,
-		"TestTable2",
-		"col1",
-		"VARCHAR"
+		t2,
+		colid,
+		TEXT
 	);
 
 	REQUIRE(result == 0);
@@ -95,9 +102,9 @@ TEST_CASE("Add Table Primary function") {
 TEST_CASE("Add Text Column") {
 	int result = SqliteHandler::DbMod::AddColumn(
 		DB,
-		"TestTable2",
-		"col2",
-		"VARCHAR"
+		t2,
+		colStr,
+		TEXT
 	);
 
 	REQUIRE(result == 0);
@@ -106,9 +113,9 @@ TEST_CASE("Add Text Column") {
 TEST_CASE("Add Int Column") {
 	int result = SqliteHandler::DbMod::AddColumn(
 		DB,
-		"TestTable2",
-		"col3",
-		"INT"
+		t2,
+		colInt,
+		INTEGER
 	);
 
 	REQUIRE(result == 0);
@@ -117,27 +124,27 @@ TEST_CASE("Add Int Column") {
 TEST_CASE("Add some Blob Columns") {
 	int result = SqliteHandler::DbMod::AddColumn(
 		DB,
-		"TestTable2",
-		"col4",
-		"BLOB"
+		t2,
+		colVecPair,
+		BLOB
 	);
 
 	REQUIRE(result == 0);
 
 	result = SqliteHandler::DbMod::AddColumn(
 		DB,
-		"TestTable2",
-		"col5",
-		"BLOB"
+		t2,
+		colVecString,
+		BLOB
 	);
 
 	REQUIRE(result == 0);
 
 	result = SqliteHandler::DbMod::AddColumn(
 		DB,
-		"TestTable2",
-		"col6",
-		"BLOB"
+		t2,
+		colVecInt,
+		BLOB
 	);
 
 	REQUIRE(result == 0);
@@ -146,11 +153,11 @@ TEST_CASE("Add some Blob Columns") {
 TEST_CASE("Add Table with foreign reference") {
 	int result = SqliteHandler::DbMod::CreateTableWithForeignKey(
 		DB,
-		"TestTable3",
-		"col1",
-		"VARCHAR",
-		"col1",
-		"VARCHAR"
+		t3,
+		colid,
+		TEXT,
+		colid,
+		TEXT
 	);
 
 	REQUIRE(result == 0);
@@ -159,25 +166,25 @@ TEST_CASE("Add Table with foreign reference") {
 TEST_CASE("Add Rows into table") {
 	int result = SqliteHandler::DataSetter::InsertRow(
 		DB,
-		"TestTable2",
-		"col1",
-		"row1"
+		t2,
+		colid,
+		row1
 	);
 	REQUIRE(result == 0);
 
 	result = SqliteHandler::DataSetter::InsertRow(
 		DB,
-		"TestTable2",
-		"col1",
-		"row2"
+		t2,
+		colid,
+		row2
 	);
 	REQUIRE(result == 0);
 
 	result = SqliteHandler::DataSetter::InsertRow(
 		DB,
-		"TestTable2",
-		"col1",
-		"row3"
+		t2,
+		colid,
+		row3
 	);
 	REQUIRE(result == 0);
 
@@ -186,11 +193,11 @@ TEST_CASE("Add Rows into table") {
 TEST_CASE("update text value in field") {
 	int result = SqliteHandler::DataSetter::StoreData(
 		DB,
-		"TestTable2",
-		"col2",
+		t2,
+		colStr,
 		str1,
-		"col1",
-		"row1"
+		colid,
+		row1
 	);
 
 	REQUIRE(result == 0);
@@ -199,11 +206,11 @@ TEST_CASE("update text value in field") {
 TEST_CASE("update Textvalue in Int Field, should return error") {
 	int result = SqliteHandler::DataSetter::StoreData(
 		DB,
-		"TestTable2",
-		"col3",
+		t2,
+		colInt,
 		str1,
-		"col1",
-		"row1"
+		colid,
+		row1
 	);
 	REQUIRE(result == 1);
 }
@@ -211,11 +218,11 @@ TEST_CASE("update Textvalue in Int Field, should return error") {
 TEST_CASE("Update Textvalue in non existent column, should return error") {
 	int result = SqliteHandler::DataSetter::StoreData(
 		DB,
-		"TestTable2",
+		t2,
 		"col50",
 		str1,
-		"col1",
-		"row1"
+		colid,
+		row1
 	);
 	REQUIRE(result == 1);
 }
@@ -223,10 +230,10 @@ TEST_CASE("Update Textvalue in non existent column, should return error") {
 TEST_CASE("Update Textvalue in non existent row, should return error") {
 	int result = SqliteHandler::DataSetter::StoreData(
 		DB,
-		"TestTable2",
+		t2,
 		"col50",
 		str1,
-		"col1",
+		colid,
 		"row50"
 	);
 	REQUIRE(result == 1);
@@ -237,11 +244,11 @@ TEST_CASE("Update int value in field") {
 
 	result = SqliteHandler::DataSetter::StoreData(
 		DB,
-		"TestTable2",
-		"col3",
-		40,
-		"col1",
-		"row1"
+		t2,
+		colInt,
+		int2,
+		colid,
+		row1
 	);
 
 	REQUIRE(result == 0);
@@ -250,19 +257,14 @@ TEST_CASE("Update int value in field") {
 TEST_CASE("Update vector with pairs value in field") {
 	int result = 0;
 
-	std::vector<std::pair<int, int>> testPairs {
-		{ 1, 2 },
-		{ 3, 4 },
-		{ 5, 6 }
-	};
 
 	result = SqliteHandler::DataSetter::StoreData(
 		DB,
-		"TestTable2",
-		"col4",
-		testPairs,
-		"col1",
-		"row1"
+		t2,
+		colVecPair,
+		pairVec1,
+		colid,
+		row1
 	);
 	REQUIRE(result == 0);
 }
@@ -270,19 +272,13 @@ TEST_CASE("Update vector with pairs value in field") {
 TEST_CASE("Update blob field with vector containing strings") {
 	int result = 0;
 
-	std::vector<std::string> testStrVec {
-		"one",
-			"two",
-			"three"
-	};
-
 	result = SqliteHandler::DataSetter::StoreData(
 		DB,
-		"TestTable2",
-		"col4",
-		testStrVec,
-		"col1",
-		"row1"
+		t2,
+		colVecString,
+		strVec1,
+		colid,
+		row1
 	);
 	REQUIRE(result == 0);
 }
@@ -290,19 +286,13 @@ TEST_CASE("Update blob field with vector containing strings") {
 TEST_CASE("Update blob field with vector containing integers") {
 	int result = 0;
 
-	std::vector<int> testIntVec {
-		1,
-			2,
-			3
-	};
-
 	result = SqliteHandler::DataSetter::StoreData(
 		DB,
-		"TestTable2",
-		"col4",
-		testIntVec,
-		"col1",
-		"row1"
+		t2,
+		colVecInt,
+		intVec1,
+		colid,
+		row1
 	);
 	REQUIRE(result == 0);
 }
@@ -312,9 +302,9 @@ TEST_CASE("Remove existing Row, should return 0") {
 
 	result = SqliteHandler::DataSetter::RemoveRow(
 		DB,
-		"TestTable2",
-		"col1",
-		"row2"
+		t2,
+		colid,
+		row2
 	);
 
 	REQUIRE(result == 0);
@@ -325,8 +315,8 @@ TEST_CASE("Remove inexistent Row, should return errorcode") {
 
 	result = SqliteHandler::DataSetter::RemoveRow(
 		DB,
-		"TestTable2",
-		"col1",
+		t2,
+		colid,
 		"row50"
 	);
 
@@ -338,20 +328,55 @@ TEST_CASE("Remove existing Row by value of non existent column, should return er
 
 	result = SqliteHandler::DataSetter::RemoveRow(
 		DB,
-		"TestTable2",
+		t2,
 		"col50",
-		"row2"
+		row2
 	);
 
 	REQUIRE(result == 1);
 }
 
-// Load data from DB
 TEST_CASE("Load String from table in database") {
 	int result = 0;
 	std::vector<std::string> data;
 
-	result = DataGetter::LoadData(DB, t2, col2, data);
+	result = DataGetter::LoadData(DB, t2, colStr, data);
 
 	REQUIRE(data[0] == str1);
+}
+
+TEST_CASE("Load Int from table in database") {
+	int result = 0;
+	std::vector<int> data;
+
+	result = DataGetter::LoadData(DB, t2, colInt, data);
+
+	REQUIRE(data[0] == int2);
+}
+
+TEST_CASE("Load vector with integers from table in database") {
+	int result = 0;
+	std::vector < std::vector<int> > data;
+
+	result = DataGetter::LoadData(DB, t2, colVecInt, data);
+
+	REQUIRE(data[0] == intVec1);
+}
+
+TEST_CASE("Load vector with string from table in database") {
+	int result = 0;
+	std::vector < std::vector<std::string> > data;
+
+	result = DataGetter::LoadData(DB, t2, colVecString, data);
+
+	REQUIRE(data[0] == strVec1);
+}
+
+TEST_CASE("Load vector with pairs from table in database") {
+	int result = 0;
+	std::vector < std::vector<std::pair<int,int>> > data;
+
+	result = DataGetter::LoadData(DB, t2, colVecPair, data);
+
+	REQUIRE(data[0] == pairVec1);
 }
