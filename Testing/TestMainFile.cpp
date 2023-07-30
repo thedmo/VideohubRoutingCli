863fb103ebddab809f26a1dfe92e41bcccdbd410
Minjaben <Benjamin@Minjaben.ch>
2023-07-30 12:36:41 +0200

    replaces testheader. Adds functionality, that TestSourcefiles can be deactivated by commenting them in this File

---
+++ Testing/TestMainFile.cpp
@@ -0,0 +1,11 @@
+#ifndef CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_MAIN
+
+#define CATCH_CONFIG_MAIN
+#endif // !CATCH_CONFIG_MAIN
+
+#include <Catch2/catch.hpp>
+
+// Include sourcefiles with tests in them
+#include <SqlAccessHandlerTests.cpp>
+#include <DataHandlerTests.cpp>
