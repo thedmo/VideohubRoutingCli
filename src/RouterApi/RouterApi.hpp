469a62d4fb52387863d2603bdb4efc0baa57a34d
kara <benjaminehauser@gmail.com>
2023-08-02 19:23:03 +0200

    adds class with methods to convert lines to relevant data

--- src/RouterApi/RouterApi.hpp
+++ src/RouterApi/RouterApi.hpp
@@ -1,4 +1,5 @@
 #include <SqliteInterface.hpp>
+#include <DataHandler.hpp>
 #include <vector>
 
 #include <TelnetClient.hpp>
@@ -6,61 +7,117 @@
 
 const int VIDEOHUB_TELNET_PORT = 9990;
 
+class Converter {
+private:
+	static int GetLineContents(std::string line, std::vector<std::string>& lineContentVec) {
+		int result = 0;
+		std::string word;
+
+		if (line.back() == '\n') {
+			line.pop_back();
+		}
+		std::stringstream line_stream(line);
+
+		while (std::getline(line_stream, word, ' ')) {
+			lineContentVec.push_back(word);
+		}
+		if (lineContentVec.empty()) return 1;
+
+		return 0;
+	}
+
+
+public:
+	static int RouteLineToRoutePair(std::string line, std::pair<int, int>& route) {
+		int result = 0;
+		std::vector<std::string> words;
+		result = GetLineContents(line, words);
+		if (result) return 1;
+
+		if (words.size() < 2) return 1;
+
+		route.first = std::stoi(words[0]);
+		route.second = std::stoi(words[1]);
+
+		return 0;
+	}
+
+	static int StringLineToData(std::string line, std::string& lineData) {
+		int result = 0;
+		std::vector<std::string> words;
+		result = GetLineContents(line, words);
+		if (result) return 1;
+
+		if (words.size() < 2) return 1;
+
+		for (size_t i = 1; i < words.size(); i++)
+		{
+			lineData += words[i];
+
+			if (i >= words.size()-1) break;
+
+			lineData += " ";
+		}
+
+		return 0;
+	}
+};
+
 class Vapi {
 public:
-  enum information_type {
-    none,
-    preamble,
-    device,
-    inputs_labels,
-    outputs_labels,
-    routing,
-    locks,
-    configuration,
-    ack,
-    end_prelude
-  };
+	enum information_type {
+		none,
+		preamble,
+		device,
+		inputs_labels,
+		outputs_labels,
+		routing,
+		locks,
+		configuration,
+		ack,
+		end_prelude
+	};
 
-  Vapi();
-  ~Vapi() {};
+	Vapi();
+	~Vapi() {};
 
-  static int AddRouter(std::string ip);
-  static int SelectRouter(std::string ip);
-  static int RemoveSelectedRouter();
-  static int GetDevices(std::string &callback);
-  static int RenameSource(int channel_number, const std::string new_name);
-  static int GetSources(std::string &callback);
-  static int RenameDestination(int channel_number, const std::string new_name);
-  static int GetDestinations(std::string &callback);
-  static int PrepareNewRoute(unsigned int destination, unsigned int source);
-  static int TakePreparedRoutes();
-  static int LockRoute(unsigned int destination);
-  static int UnlockRoute(unsigned int destination);
-  static int GetRoutes(std::string &callback);
-  static int MarkRouteForSaving(int destination);
-  static int MarkRoutes2(std::vector<int> destinations);
-  static int SaveRoutes(std::string routing_name);
-  static int GetSavedRoutes(std::string &callback);
-  static int LoadRoutes(std::string name);
+	static int AddRouter(std::string ip);
+	static int SelectRouter(std::string ip);
+	static int RemoveSelectedRouter();
+	static int GetDevices(std::string& callback);
+	static int RenameSource(int channel_number, const std::string new_name);
+	static int GetSources(std::string& callback);
+	static int RenameDestination(int channel_number, const std::string new_name);
+	static int GetDestinations(std::string& callback);
+	static int PrepareNewRoute(unsigned int destination, unsigned int source);
+	static int TakePreparedRoutes();
+	static int LockRoute(unsigned int destination);
+	static int UnlockRoute(unsigned int destination);
+	static int GetRoutes(std::string& callback);
+	static int MarkRouteForSaving(int destination);
+	static int MarkRoutes2(std::vector<int> destinations);
+	static int SaveRoutes(std::string routing_name);
+	static int GetSavedRoutes(std::string& callback);
+	static int LoadRoutes(std::string name);
 
-  static int GetStatus(std::string ip, std::unique_ptr<device_data> &_data);
+	static int GetStatus(std::string ip, std::unique_ptr<device_data>& _data);
 
-  static std::vector<std::string> GetErrorMessages();
+	static std::vector<std::string> GetErrorMessages();
 
-  static const int ROUTER_API_NOT_OK = 1;
-  static const int ROUTER_API_OK = 0;
+	static const int ROUTER_API_NOT_OK = 1;
+	static const int ROUTER_API_OK = 0;
 
 private:
-  static std::vector<std::string> m_err_msgs;
-  static int AddToTrace(std::string);
-  static int AddToTrace(std::string err, std::vector<std::string> err_list);
+	static std::vector<std::string> m_err_msgs;
+	static int AddToTrace(std::string);
+	static int AddToTrace(std::string err, std::vector<std::string> err_list);
 
-  static int GetInformationType(std::string line, information_type &type);
-  static int ExtractInformation(std::string, std::unique_ptr<device_data> &_data);
+	static int GetInformationType(std::string line, information_type& type);
+	static int ExtractInformation(std::string, std::unique_ptr<device_data>& _data);
 
-  static int GetDeviceInformation(std::string, std::unique_ptr<device_data> &_data);
+	static int GetDeviceInformation(std::string, std::unique_ptr<device_data>& _data);
 
-  static int check_channel_number(int num);
+	static int check_channel_number(int num);
 
-  static vdb m_database;
+	static vdb m_database;
 };
