fcd5e5f2d02a8ca0c1484b8053a677f66ab8fb94
kara <benjaminehauser@gmail.com>
2023-08-05 20:14:37 +0200

    adds method to initialize storage and new test

--- Testing/RouterApiTests.cpp
+++ Testing/RouterApiTests.cpp
@@ -1,6 +1,50 @@
 #include <catch2/catch.hpp>
 #include <RouterApi.hpp>
 #include <SqliteInterface.hpp>
+#include <DataHandler.hpp>
+#include <MockupServer.h>
+
+int InitializeStorage() {
+	int result = 0;
+
+	result = DataHandler::ClearStorageFile("RouterDb");
+	if (result) return 1;
+
+	result = DataHandler::ClearStorageFile("router");
+	if (result) return 1;
+
+	result = DataHandler::InitializeStorage();
+	if (result) return 1;
+
+	vdb dataBase;
+	VhubMockup::StartServer();
+
+	return 0;
+}
+
+int AddDevicesToStorage() {
+	int result = 0;
+
+	Vapi::AddRouter("127.0.0.1");
+
+	std::unique_ptr<device_data> device;
+	DataHandler::GetDataOfSelectedDevice(device);
+
+	device->ip = "127.0.0.2";
+	DataHandler::AddDevice(device);
+	DataHandler::SelectDevice(device);
+	DataHandler::UpdateSelectedDeviceData(device);
+
+	std::unique_ptr<device_data> device2;
+	DataHandler::GetDataOfSelectedDevice(device2);
+
+	device2->ip = "127.0.0.3";
+	DataHandler::AddDevice(device2);
+	DataHandler::SelectDevice(device2);
+	DataHandler::UpdateSelectedDeviceData(device2);
+
+	return 0;
+}
 
 TEST_CASE("Cenvert line with two numbers on it into a std pair of two ints") {
 	int result = 0;
@@ -38,20 +82,22 @@ TEST_CASE("Get Status of device by ip address") {
 TEST_CASE("Add new router to storage") {
 	int result = 0;
 
-	result = DataHandler::ClearStorageFile("RouterDb");
-	REQUIRE(result == 0);
-
-	result = DataHandler::ClearStorageFile("router");
-	REQUIRE(result == 0);
-
-	result = DataHandler::InitializeStorage();
-	REQUIRE(result == 0);
+	result = InitializeStorage();
 
 	vdb dataBase;
 
 	std::string ip = "127.0.0.1";
 	result = Vapi::AddRouter(ip);
 	REQUIRE(result == 0);
+}
 
-	
+TEST_CASE("Select a Router from the devices list") {
+	InitializeStorage();
+	AddDevicesToStorage();
+
+	int result = 0;
+
+
+
+	REQUIRE(result == 0);
 }
